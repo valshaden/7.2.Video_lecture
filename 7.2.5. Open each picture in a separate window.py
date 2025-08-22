e22b9d7ae86d43ac3599095be1be5fabb4f58ced
valshaden <valshaden@yandex.ru>
2025-08-22 18:27:02 +0300

    + Задаем размер с помощью спинбоксов

--- 7.2.5. Open each picture in a separate window.py
+++ 7.2.5. Open each picture in a separate window.py
@@ -62,13 +62,13 @@ progress.pack(padx=10, pady=5)
 # Ширина
 width_label = ttk.Label(text="Ширина:")
 width_label.pack(side='left', padx=(10, 0))
-width_spinbox = ttk.Spinbox(from_=200, to=500, increment=50, width=5)
+width_spinbox = ttk.Spinbox(from_=256, to=1024, increment=32, width=5)
 width_spinbox.pack(side='left', padx=(0, 10))
 
 # Высота
 height_label = ttk.Label(text="Высота:")
 height_label.pack(side='left', padx=(10, 0))
-height_spinbox = ttk.Spinbox(from_=200, to=500, increment=50, width=5)
+height_spinbox = ttk.Spinbox(from_=256, to=1024, increment=32, width=5)
 height_spinbox.pack(side='left', padx=(0, 10))
 
 window.mainloop()
